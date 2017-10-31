48a69eb80bcd764e9e7ac54d78bc5c8b12e7e610
Luiz França <luizdaniel.r.f@gmail.com>
2017-10-31 12:36:07 -0300

    update intent

--- index.js
+++ index.js
@@ -23,7 +23,7 @@ app.intent('sayNumber',
 		"tell me about cesar"]
   },
   function(request,response) {
-    var number = request.slot('number');
+    // var number = request.slot('number');
     response.say("We are a private innovation centre that creates products, services and businesses involving Communication and Information Technologies (ICTs). Since 1996, we develop solutions that permeate the whole process of generating innovation in and with ICTs – from idea development, passing through conception and prototyping, up to the execution of projects for organizations from various sectors, such as telecom, electronics, defence, commercial automation, finances, logistics, energy, health and agribusiness. In 2016, CESAR’s innovation project sales surpassed R$ 78 million.");
   }
 );
