9edbad29912faafe147b6a7ed307d65f425b4cdf
Luiz França <luizdaniel.r.f@gmail.com>
2017-10-31 12:15:39 -0300

    update responsse

--- index.js
+++ index.js
@@ -28,7 +28,7 @@ app.intent('sayNumber',
   },
   function(request,response) {
     var number = request.slot('number');
-    response.say("You asked for the number "+number);
+    response.say("Somos um centro privado de inovação que cria produtos, serviços e negócios com Tecnologias da Informação e Comunicação (TICs). Desde 1996, desenvolvemos soluções em todo o processo de geração de inovação em e com TICs - desde o desenvolvimento da ideia, passando pela concepção e prototipação, até a execução de projetos para empresas dos mais diversos setores, como telecomunicações, eletroeletrônicos, defesa, automação comercial, financeiro, logística, energia, saúde e agronegócio. Em 2014, o CESAR superou a marca de R$ 90 milhões em vendas de projetos de inovação.");
   }
 );
 
