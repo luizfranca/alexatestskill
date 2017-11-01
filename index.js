2149f3bf76ac5a13a5a4e6596d1aa6ed06020dbd
Luiz França <luizdaniel.r.f@gmail.com>
2017-11-01 07:53:07 -0300

    changing the name of the intent and adding new utterances

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ var app = new alexa.app( 'alexatestskill' );
 
 
 app.launch( function( request, response ) {
-	response.say( 'Welcome to your test skill' ).reprompt( 'Way to go. You got it to run. Bad ass.' ).shouldEndSession( false );
+	response.say( 'Welcome to Cesar' ).reprompt( 'Way to go. You got it to run. Bad ass.' ).shouldEndSession( false );
 } );
 
 
@@ -17,14 +17,15 @@ app.error = function( exception, request, response ) {
 	response.say( 'Sorry an error occured ' + error.message);
 };
 
-app.intent('sayNumber',
+app.intent('cesarinfo',
   {
   	// "slots":{"number":"NUMBER"}
 	"utterances":[ 
-		"tell me about cesar"]
+		"tell me about cesar",
+		"tell me you",
+		"who are you"]
   },
   function(request,response) {
-    // var number = request.slot('number');
     response.say("We are a private innovation centre that creates products, services and businesses involving Communication and Information Technologies (ICTs). Since 1996, we develop solutions that permeate the whole process of generating innovation in and with ICTs – from idea development, passing through conception and prototyping, up to the execution of projects for organizations from various sectors, such as telecom, electronics, defence, commercial automation, finances, logistics, energy, health and agribusiness. In 2016, CESAR’s innovation project sales surpassed R$ 78 million.");
   }
 );
