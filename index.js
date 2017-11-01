e1fcdb8614a258eff43116605a2ecc004d146510
Luiz França <luizdaniel.r.f@gmail.com>
2017-11-01 14:26:49 -0300

    changing the text on the card

--- index.js
+++ index.js
@@ -27,14 +27,17 @@ app.intent('cesarinfo',
   },
   function(request,response) {
   	// response.say()
+
+  	message = "We are a private innovation centre that creates products, services and businesses involving Communication and Information Technologies (ICTs). Since 1996, we develop solutions that permeate the whole process of generating innovation in and with ICTs – from idea development, passing through conception and prototyping, up to the execution of projects for organizations from various sectors, such as telecom, electronics, defence, commercial automation, finances, logistics, energy, health and agribusiness. In 2016, CESAR’s innovation project sales surpassed R$ 78 million."
+
   	card = {
       "type": "Simple",
-      "title": "Horoscope",
-      "content": "Today will provide you a new learning opportunity.  Stick with it and the possibilities will be endless."
+      "title": "CESAR Info",
+      "content": message
     }
 
   	response.card(card)
-    response.say("We are a private innovation centre that creates products, services and businesses involving Communication and Information Technologies (ICTs). Since 1996, we develop solutions that permeate the whole process of generating innovation in and with ICTs – from idea development, passing through conception and prototyping, up to the execution of projects for organizations from various sectors, such as telecom, electronics, defence, commercial automation, finances, logistics, energy, health and agribusiness. In 2016, CESAR’s innovation project sales surpassed R$ 78 million.");
+    response.say(message);
   }
 );
 
