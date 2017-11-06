1f8a6a8e855c4c01b91b4d5362284a4eae8e762b
Luiz França <luizdaniel.r.f@gmail.com>
2017-11-06 09:32:54 -0300

    Testing

--- index.js
+++ index.js
@@ -6,37 +6,84 @@ var app = new alexa.app( 'alexatestskill' );
 
 
 app.launch( function( request, response ) {
-	response.say( 'Welcome to Cesar' ).reprompt( 'Way to go. \
-		You got it to run. Bad ass.' ).shouldEndSession( false );
+    response.say( 'Welcome to Cesar' ).reprompt( 'Way to go. \
+        You got it to run. Bad ass.' ).shouldEndSession( false );
 } );
 
 
 app.error = function( exception, request, response ) {
-	console.log(exception)
-	console.log(request);
-	console.log(response);	
-	response.say( 'Sorry an error occured ' + error.message);
+    console.log(exception)
+    console.log(request);
+    console.log(response);  
+    response.say( 'Sorry an error occured ' + error.message);
 };
 
 app.intent('cesarinfo',
   {
-	"utterances":[ 
-		"tell me about cesar",
-		"tell me you",
-		"who are you",
-		"tell me about yourself"]
+    "utterances":[ 
+        "tell me about cesar",
+        "tell me you",
+        "who are you",
+        "tell me about yourself"]
   },
   function(request,response) {
-  	message = "We are a private innovation centre that creates products, services and businesses involving Communication and Information Technologies (ICTs). Since 1996, we develop solutions that permeate the whole process of generating innovation in and with ICTs – from idea development, passing through conception and prototyping, up to the execution of projects for organizations from various sectors, such as telecom, electronics, defence, commercial automation, finances, logistics, energy, health and agribusiness. In 2016, CESAR’s innovation project sales surpassed R$ 78 million."
+    message = "We are a private innovation centre that creates products, services and businesses involving Communication and Information Technologies (ICTs). Since 1996, we develop solutions that permeate the whole process of generating innovation in and with ICTs – from idea development, passing through conception and prototyping, up to the execution of projects for organizations from various sectors, such as telecom, electronics, defence, commercial automation, finances, logistics, energy, health and agribusiness. In 2016, CESAR’s innovation project sales surpassed R$ 78 million."
 
-  	card = {
+    card = {
       "type": "simple",
       "title": "CESAR Info",
       "content": message
     }
 
 
-  	response.card(card);
+    response.card(card);
+    response.say(message);
+  }
+);
+
+app.intent('cesarinfo2',
+  {
+    "utterances":[ 
+        "show info"]
+  },
+  function(request,response) {
+    message = "This is the second message"
+
+    card = {
+      "type": "simple",
+      "title": "CESAR Info",
+      "content": message
+    }
+
+    dir = 
+        {
+          "type": "Display.RenderTemplate",
+          "template": {
+            "type": "BodyTemplate1",
+            "token": "string",
+            "backButton": "VISIBLE",
+            "backgroundImage": "Image",
+            "title": "string",
+            "textContent": {
+              "primaryText": {
+                "text": "string",
+                "type": "string"
+              },
+              "secondaryText": {
+                "text": "string",
+                "type": "string"
+              },
+              "tertiaryText": {
+                "text": "string",
+                "type": "string"
+              }
+            }
+          }
+        }
+
+    // response.directives = [dir];
+    // response.card(card);
+    response.directive(dir)
     response.say(message);
   }
 );
